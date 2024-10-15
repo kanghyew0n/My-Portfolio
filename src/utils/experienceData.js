71bc1ee9652a8b60f25df065250b68aab267ae04
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-10-15 23:16:38 +0900

    add: 프로젝트 데이터 및 이미지 추가

--- src/utils/experienceData.js
+++ src/utils/experienceData.js
@@ -1,5 +1,11 @@
+import Link from "next/link";
+
+const webDesignLink = 'https://drive.google.com/file/d/1nOb7aewK6vmiag8W9qRTn1G9fWmZNGkD/view'
+const mobileDesignLink = 'https://drive.google.com/file/d/1jyAucYf00cczoWrV8MLpclZj0x_u4uJL/view'
+
 export const CAREER = [
   {
+    id: '202301-202405',
     company: "(주)스퀘어스 | 큐샵 Qshop",
     info: [{
       title: "2023년 1월 ~ 2024년 5월 (1년 5개월)"
@@ -8,16 +14,16 @@ export const CAREER = [
       link: 'https://qshop.ai/'
     }],
     contents: <>
-      <p className="text-lg font-light">
+      <p className="text-lg font-light mb-2">
         큐샵은 누구나 코딩없이 웹사이트를 쉽게 생성할 수 있는 서비스로 핵심
         기능인 <b>노코드 웹빌더 에디터와 큐샵 자체 홈페이지를 개발</b>하였습니다.
         큐샵의 홈페이지와 에디터, 두가지 서비스 모두 <b>기획 단계부터 참여하여
           런칭까지</b> 함께 하였습니다.
       </p>
       <p className="text-lg font-light">
-        커밋 컨벤션 문서화, 코드리뷰의 도입 등과 같은 노력으로 협업 환경을
-        개선하였고, 성능 개선의 필요성을 제안하여 번들 분석 및 최적화 작업을
-        진행하여 프로젝트의 성장에 기여하였습니다.
+        <b>커밋 컨벤션 문서화, 코드리뷰의 도입</b> 등과 같은 노력으로 협업 환경을
+        개선하였고, <b>성능 개선의 필요성을 제안</b>하여 <b>번들 분석 및 최적화 작업을
+          진행</b>하여 프로젝트의 성장에 기여하였습니다.
       </p>
     </>
   }
@@ -25,19 +31,21 @@ export const CAREER = [
 
 export const EDUCATION = [
   {
+    id: '202204-202210',
     name: ' 코드스테이츠 (Code States)',
     info: [{
       title: "2022년 04월 - 2022년 10월"
     }, {
       title: '🔗 코드스테이츠 홈페이지',
-      link: ''
+      link: 'https://www.codestates.com/'
     }],
-    description: 'Advanced software engineering, Immersive program (39기)'
+    contents: <p className="text-lg font-light">Advanced software engineering, Immersive program (39기)</p>
   },
   {
+    id: '201802-20238',
     name: '한국공학대학교',
     info: [{
-      title: "2018.03 - 2022.08"
+      title: "2018.03 - 2023.08"
     }],
     description: 'IT경영 전공 | 미디어디자인공학 부전공',
     contents: <>
@@ -47,8 +55,8 @@ export const EDUCATION = [
       </p>
       <p className="text-lg font-light">
         ⦁ 미디어디자인공학과에서 웹/앱 프로토타입 디자인 및 UX를
-        공부했습니다. (웹디자인 / 모바일 디자인)
+        공부했습니다. ( <Link href={webDesignLink} target="_blank">웹디자인</Link> / <Link href={mobileDesignLink} target="_blank">모바일 디자인</Link>)
       </p>
     </>
-  }
+  },
 ]
---
+++ src/utils/projectData.js
@@ -0,0 +1,86 @@
+
+export const PROJECT = [
+  {
+    id: 'qshop-editor',
+    name: '큐샵 에디터 ',
+    info: [{
+      title: '(주) 스퀘어스',
+    },
+    {
+      title: 'FE 3-4명, BE 1-2명',
+    }],
+    description: '드래그 드롭으로 웹사이트를 제작할 수 있도록 도와주는 노코드 웹빌더 서비스로 큐샵의 핵심 기능 중 하나입니다.',
+    result: [],
+    troubleShooting: [{
+      title: '',
+      link: ''
+    }],
+    techStacks: ['React', 'Next Js', 'Scss', 'Recoil', 'ContextAPI', 'React-Query'],
+    images: [{
+      path: '/assets/project/qshop_editor_pc.png',
+      alt: '큐샵 에디터 피씨'
+    }, {
+      path: '/assets/project/qshop_editor_mo.png',
+      alt: '큐샵 에디터 모바일'
+    }]
+  },
+  {
+    id: 'qshop-homepage',
+    name: '큐샵 홈페이지',
+    info: [{
+      title: '(주) 스퀘어스',
+    },
+    {
+      title: 'FE 2명, BE 1-2명',
+    }],
+    description: '큐샵 자체 홈페이지로 가입, 구독결제, 사이트 템플릿 등을 제공합니다.',
+    result: [],
+    troubleShooting: [{
+      title: '',
+      link: ''
+    }],
+    techStacks: ['React', 'Next Js', 'Emotion', 'Recoil', 'React-Query', 'React-Hook-Form'],
+    images: [{
+      path: '/assets/project/qshop_editor_pc.png',
+      alt: '큐샵 에디터 피씨'
+    }, {
+      path: '/assets/project/qshop_editor_mo.png',
+      alt: '큐샵 에디터 모바일'
+    }]
+  }, {
+    id: 'bead-buddies',
+    name: 'Bead Buddies (비즈발 도안 제작소)',
+    info: [{
+      title: '2024.09 - 진행중',
+    },
+    {
+      title: '개인 프로젝트',
+    },
+    {
+      title: '🔗 Github',
+      link: 'https://github.com/kanghyew0n/bead-buddies'
+    },
+    {
+      title: '🔴 BeadBuddies 사이트',
+      link: 'https://github.com/kanghyew0n/bead-buddies'
+    },
+    {
+      title: '🎨 storybook',
+      link: 'https://github.com/kanghyew0n/bead-buddies'
+    }],
+    description: `비즈발의 도안을 제작할 수 있는 서비스입니다. \n 비즈공예가 평소 취미였던 저는 비즈발 이라는 것을 접하게 되었고 만들고싶다는 생각을 했습니다. 하지만 비즈발 도안 제작을 엑셀 시트나 수작업으로 해야 한다는 사실을 알게 되었고 간단하게 도안을 제작할 수 있는 서비스를 만들게 되었습니다.`,
+    result: [],
+    troubleShooting: [{
+      title: '',
+      link: ''
+    }],
+    techStacks: ['React', 'Next Js', 'Scss', 'Recoil', 'ContextAPI', 'React-Query'],
+    images: [{
+      path: '/assets/project/qshop_editor_pc.png',
+      alt: '큐샵 에디터 피씨'
+    }, {
+      path: '/assets/project/qshop_editor_mo.png',
+      alt: '큐샵 에디터 모바일'
+    }]
+  }
+]
