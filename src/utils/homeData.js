910a2c69a3a8cac5623e931fd46a27f85cdcc7a4
kanghyew0n <104333249+kanghyew0n@users.noreply.github.com>
2024-10-29 22:45:08 +0900

    refactor: 홈페이지 데이터 파일명 변경

---
+++ src/utils/homeData.js
@@ -0,0 +1,37 @@
+export const ABOUT = {
+    problemSolver: {
+        icon: "🧙‍♀️",
+        keyWord: "Problem Solver",
+        subTitle: "문제를 찾아 공유하고, 해결하는 것을 좋아합니다.",
+        content:
+            "웹 빌더 사용 중 발생하는 버벅거림을 분석한 결과, 리플로우와 리페인트가 강제로 일어나는 것을 발견했습니다. 이를 통해 성능 최적화의 필요성을 느끼고, 프론트 팀에 문제를 공유하여 성능 최적화 스프린트를 진행했습니다. 그 결과 버벅거리는 문제뿐만 아니라 초기 로딩속도도 약 30% 개선되었습니다.",
+    },
+
+    processImprover: {
+        icon: "🔄",
+        keyWord: "Process Improver",
+        subTitle: "프로세스를 개선하고, 더 나은 개발 문화를 만듭니다.",
+        content:
+            "런칭 직전에 코드 리뷰를 잠시 중단한 결과, 무분별한 핫픽스가 잦아지는 문제가 발생했습니다. 이를 해결하기 위해 프론트 팀에 코드 리뷰를 다시 도입하자고 제안하고, 커밋 컨벤션 문서도 업데이트했습니다. 이후 핫픽스 빈도가 크게 줄어들었고, 체계적인 코드 리뷰의 중요성을 다시 깨달았습니다. 이 경험을 통해 팀의 코드 품질과 안정성을 높이는 데 기여할 수 있었습니다.",
+    },
+
+    effectiveCommunicator: {
+        icon: "💬",
+        keyWord: "Effective Communicator",
+        subTitle: "프론트엔드 개발자에게 커뮤니케이션 능력은 필수라고 생각합니다. ",
+        content:
+            "다양한 팀원들과 원활하게 소통하기 위해서는 명확한 의사 전달과 함께 존중하는 태도가 필요합니다. 저는 이러한 원칙을 바탕으로 동료들과의 협업에서 긍정적인 피드백을 꾸준히 받아왔습니다.",
+    },
+}
+
+export const TECH_STACK = {
+    core: ["React", "Next Js", "TypeScript"],
+    libraries: ["Scss", "Emotion", "Recoil", "React-Query", "React-Hook-Form"],
+
+}
+
+export const FOOTER = {
+    github: "https://github.com/kanghyew0n",
+    blog: "https://velog.io/@kanghyeron/posts",
+    linkedin: 'https://www.linkedin.com/in/%ED%98%9C%EC%9B%90-%EA%B0%95-9650b827a/'
+}
